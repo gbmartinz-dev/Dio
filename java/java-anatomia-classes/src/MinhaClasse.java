3d2b280c930ed5d20d9ec647d654c57737358145
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-04-05 12:17:09 -0300

    Continuando os estudos sobre Anatomia das classes

--- java/java-anatomia-classes/src/MinhaClasse.java
+++ java/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,16 @@
 public class MinhaClasse {
   public static void main (String[] args) {
-    System.out.print( "Olá pessoal, sejam Bem-vindos!" );
+    System.out.println( "Olá tudo bem com você? Seja Bem-vindo! Ao Sistema de ensino" );
+    String primeiroNome = "Gabriel";
+    String segundoNome = "Martins";
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    int idade = 26;
+    int ano = 2025;
+    boolean ativoAtualmente = true;
+    System.out.println(nomeCompleto + " que está cadastrado no " + ano +  " e tem " + idade + " anos, está ativo atualmente? " + ativoAtualmente);
   }
+  public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do sistema: " + primeiroNome.concat( " ").concat(segundoNome);
+  }
+
 }
